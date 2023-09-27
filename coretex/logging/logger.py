17c2a15f74a6b515edc0ad2391ea42fdaef815dd
Dusko Mirkovic <dmirkovic@biomech.us>
2023-09-27 13:28:20 +0200

    CTX-4755: Move upload worker to a separate file

--- coretex/logging/logger.py
+++ coretex/logging/logger.py
@@ -17,18 +17,15 @@
 
 from __future__ import annotations
 
-from typing import Optional, Any, List
-from threading import Lock, Thread
+from typing import Optional, Any
+from threading import Lock
 from logging import LogRecord, StreamHandler
-from concurrent.futures import ThreadPoolExecutor
 
-import time
 import sys
-import logging
 
 from .log import Log
 from .log_severity import LogSeverity
-from ..networking import networkManager, RequestType
+from ._upload_worker import LoggerUploadWorker
 
 
 # Logs from library that is being used for making api requests is causing project to freeze because
@@ -39,95 +36,6 @@ IGNORED_LOGGERS = [
     "coretexnode",
     "werkzeug"
 ]
-MAX_WAIT_TIME_BEFORE_UPDATE = 5
-
-
-class _LoggerUploadWorker(Thread):
-
-    """
-        Not intended for outside use
-
-        A worker thread which is constantly running and
-        uploading logs to Coretex backend every 5 seconds
-
-        If the upload request fails the wait time is doubled
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-
-        self.setDaemon(True)
-        self.setName("LoggerUploadWorker")
-
-        # Task run for which the logs are uploaded
-        self._taskRunId: Optional[int] = None
-
-        self.__queue = ThreadPoolExecutor(max_workers = 1, thread_name_prefix = "LogQueue")
-        self.__waitTime = MAX_WAIT_TIME_BEFORE_UPDATE
-        self.__pendingLogs: List[Log] = []
-
-    def add(self, log: Log) -> None:
-        self.__queue.submit(self.__pendingLogs.append, log)
-
-    def uploadLogs(self) -> bool:
-        def worker() -> bool:
-            if len(self.__pendingLogs) == 0:
-                return True
-
-            if self._taskRunId is None:
-                raise ValueError(">> [Coretex] Tried to upload logs but no Task is being executed")
-
-            response = networkManager.genericJSONRequest(
-                endpoint = "model-queue/add-console-log",
-                requestType = RequestType.post,
-                parameters = {
-                    "model_queue_id": self._taskRunId,
-                    "logs": [log.encode() for log in self.__pendingLogs]
-                }
-            )
-
-            # Only clear logs if they were successfully uploaded to coretex
-            if not response.hasFailed():
-                self.__pendingLogs.clear()
-
-            return not response.hasFailed()
-
-        future = self.__queue.submit(worker)
-
-        exception = future.exception()
-        if exception is not None:
-            logging.getLogger("coretexpylib").debug(">> [Coretex] Failed to upload logs", exc_info = exception)
-            return False
-
-        return future.result()
-
-    def run(self) -> None:
-        while True:
-            time.sleep(self.__waitTime)
-
-            # Check if logger is attached to a run
-            if self._taskRunId is None:
-                continue
-
-            try:
-                success = self.uploadLogs()
-            except:
-                success = False
-
-            if success:
-                # If upload of logs was success reset wait time
-                self.__waitTime = MAX_WAIT_TIME_BEFORE_UPDATE
-            else:
-                # If upload of logs failed, double the wait time
-                self.__waitTime *= 2
-
-    def reset(self) -> None:
-        self.__queue.shutdown(wait = True)
-        self.__queue = ThreadPoolExecutor(max_workers = 1, thread_name_prefix = "LogQueue")
-
-        self._taskRunId = None
-        self.__waitTime = MAX_WAIT_TIME_BEFORE_UPDATE
-        self.__pendingLogs.clear()
 
 
 class LogHandler(StreamHandler):
@@ -153,7 +61,7 @@ class LogHandler(StreamHandler):
     def __init__(self, stream: Any) -> None:
         super().__init__(stream)
 
-        self.__uploadWorker = _LoggerUploadWorker()
+        self.__uploadWorker = LoggerUploadWorker()
         self.__uploadWorker.start()
 
     @property
@@ -170,7 +78,7 @@ class LogHandler(StreamHandler):
 
         old = self.__uploadWorker
 
-        self.__uploadWorker = _LoggerUploadWorker()
+        self.__uploadWorker = LoggerUploadWorker()
         self.__uploadWorker._taskRunId = old._taskRunId
         self.__uploadWorker.start()
 
