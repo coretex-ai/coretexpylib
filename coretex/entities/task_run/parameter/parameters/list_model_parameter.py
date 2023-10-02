c2665730261e0e94c4ebc25dfd99357f75488969
Dusko Mirkovic <dmirkovic@biomech.us>
2023-10-02 13:14:04 +0200

    CTX-4720: Added support for model, model list, and dataset list parameters

--- coretex/entities/task_run/parameter/parameters/list_model_parameter.py
+++ coretex/entities/task_run/parameter/parameters/list_model_parameter.py
@@ -15,17 +15,25 @@
 #     You should have received a copy of the GNU Affero General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .int_parameter import IntParameter
-from .float_parameter import FloatParameter
-from .str_parameter import StrParameter
-from .bool_parameter import BoolParameter
-from .dataset_parameter import DatasetParameter
-from .model_parameter import ModelParameter
-from .imu_vectors_parameter import IMUVectorsParameter
-from .enum_parameter import EnumParameter
-from .list_int_parameter import ListIntParameter
-from .list_float_parameter import ListFloatParameter
-from .list_str_parameter import ListStrParameter
-from .list_dataset_parameter import ListDatasetParameter
-from .list_model_parameter import ListModelParameter
-from .list_enum_parameter import ListEnumParameter
+from typing import Any, List, Optional
+
+from coretex.entities.project import ProjectType
+
+from ..base_list_parameter import BaseListParameter
+from ....model import Model
+
+
+class ListModelParameter(BaseListParameter[List[int]]):
+
+    @property
+    def listTypes(self) -> List[type]:
+        return [int]
+
+    def parseValue(self, type_: ProjectType) -> Optional[Any]:
+        if self.value is None:
+            return None
+
+        return [
+            Model.fetchById(modelId)
+            for modelId in self.value
+        ]
