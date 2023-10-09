0ceda3103ac95db4c2f2a9669a1e349200f3030d
Vuk Manojlovic <vmanojlovic@biomech.us>
2023-10-09 11:10:36 +0200

    CTX-4588: Removed key descriptor

--- coretex/entities/task_run/task_run.py
+++ coretex/entities/task_run/task_run.py
@@ -157,7 +157,6 @@ class TaskRun(NetworkObject, Generic[DatasetType]):
         descriptors["projectType"] = KeyDescriptor("project_task", ProjectType)
         descriptors["taskId"] = KeyDescriptor("sub_project_id")
         descriptors["taskName"] = KeyDescriptor("sub_project_name")
-        descriptors["entryPoint"] = KeyDescriptor("entry_point")
 
         # private properties of the object should not be encoded
         descriptors["__parameters"] = KeyDescriptor(isEncodable = False)
