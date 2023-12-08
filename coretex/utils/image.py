f30d32bf3c49a6e5a27b1c5461802d0d9b47b86c
Vuk Manojlovic <vmanojlovic@biomech.us>
2023-12-08 16:24:53 +0100

    CTX-4912: Added resizeWithPadding function

--- coretex/utils/image.py
+++ coretex/utils/image.py
@@ -15,8 +15,30 @@
 #     You should have received a copy of the GNU Affero General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .number import mathematicalRound
-from .file import guessMimeType, InvalidFileExtension
-from .date import DATE_FORMAT, TIME_ZONE
-from .hash import hashCacheName
-from .image import resizeWithPadding
+from typing import Tuple
+
+from PIL import Image
+
+import numpy as np
+
+
+def resizeWithPadding(
+    image: np.ndarray,
+    newShape: Tuple[int, int]
+) -> Tuple[np.ndarray, Tuple[int, int]]:
+
+    originalShape = (image.shape[1], image.shape[0])
+    ratio = max(newShape) / max(originalShape)
+    newSize = tuple([int(x * ratio) for x in originalShape])
+    resizedImage = Image.fromarray(image).resize(newSize)
+
+    deltaW = newShape[0] - newSize[0]
+    deltaH = newShape[1] - newSize[1]
+
+    top = deltaH // 2
+    left = deltaW // 2
+
+    paddedImage = Image.new(resizedImage.mode, (newShape[1], newShape[0]), 0)
+    paddedImage.paste(resizedImage, (left, top))
+
+    return np.array(paddedImage), (top, left)
