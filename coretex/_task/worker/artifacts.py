f637fd3cfc99cca7c9fbef7f944fcf324ada5e2f
Vuk Manojlovic <vmanojlovic@biomech.us>
2024-02-01 10:58:47 +0100

    CTX-5350: Added try except around createArtifacts in artifacts.py:track in order to catch bug during automatic artifact upload

--- coretex/_task/worker/artifacts.py
+++ coretex/_task/worker/artifacts.py
@@ -75,7 +75,13 @@ def track(taskRun: TaskRun) -> Iterator[FileEventHandler]:
 
         for index, artifactPath in enumerate(eventHandler.artifactPaths):
             logging.getLogger("coretexpylib").debug(f">> [Coretex] Uploading {index + 1}/{len(eventHandler.artifactPaths)} - \"{artifactPath}\"")
-            artifact = taskRun.createArtifact(artifactPath, str(artifactPath.relative_to(root)))
+
+            try:
+                artifact = taskRun.createArtifact(artifactPath, str(artifactPath.relative_to(root)))
+                if artifact is None:
+                    logging.getLogger("coretexpylib").debug(f"\tFailed to upload artifact")
+            except Exception as e:
+                logging.getLogger("coretexpylib").error(f"Error while creating artifact: {e}")
 
             if artifact is not None:
                 logging.getLogger("coretexpylib").debug(f"\tSuccessfully uploaded artifact")
