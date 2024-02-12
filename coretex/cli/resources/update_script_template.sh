d8a1db688e7a1348dba478220a275d4cb3453b58
Bogdan Tintor <btintor@biomech.us>
2024-02-12 13:29:28 +0100

    cli-hotfix: changes requested

--- coretex/cli/resources/update_script_template.sh
+++ coretex/cli/resources/update_script_template.sh
@@ -9,6 +9,7 @@ SERVER_URL={serverUrl}
 STORAGE_PATH={storagePath}
 NODE_ACCESS_TOKEN={nodeAccessToken}
 NODE_MODE={nodeMode}
+MODEL_ID={modelId}
 CONTAINER_NAME={containerName}
 NETWORK_NAME={networkName}
 RESTART_POLICY={restartPolicy}
@@ -99,7 +100,7 @@ function start_node {{
     $DOCKER_PATH network create --driver bridge $NETWORK_NAME
 
     echo "Starting the node with the latest image"
-    $DOCKER_PATH run -d --env "CTX_API_URL=$SERVER_URL" --env "CTX_STORAGE_PATH=$STORAGE_PATH" --env "CTX_NODE_ACCESS_TOKEN=$NODE_ACCESS_TOKEN" --env "CTX_NODE_MODE=$NODE_MODE" --restart $RESTART_POLICY -p $PORTS --cap-add $CAP_ADD --network "$NETWORK_NAME" --memory $RAM_MEMORY --memory-swap $SWAP_MEMORY --shm-size $SHARED_MEMORY --name "$CONTAINER_NAME" "$IMAGE"
+    $DOCKER_PATH run -d --env "CTX_API_URL=$SERVER_URL" --env "CTX_STORAGE_PATH=$STORAGE_PATH" --env "CTX_NODE_ACCESS_TOKEN=$NODE_ACCESS_TOKEN" --env "CTX_NODE_MODE=$NODE_MODE" --env "CTX_MODEL_ID=$MODEL_ID" --restart $RESTART_POLICY -p $PORTS --cap-add $CAP_ADD --network "$NETWORK_NAME" --memory $RAM_MEMORY --memory-swap $SWAP_MEMORY --shm-size $SHARED_MEMORY --name "$CONTAINER_NAME" "$IMAGE"
 }}
 
 # Define function to update node
