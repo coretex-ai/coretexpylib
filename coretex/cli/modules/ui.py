1fa2f1f85179ee600e00abff850cbbddc57e953b
Bogdan Tintor <btintor@biomech.us>
2024-02-13 16:35:02 +0100

    CTX-5467: Added some spacing.

--- coretex/cli/modules/ui.py
+++ coretex/cli/modules/ui.py
@@ -9,10 +9,11 @@ from .node_mode import NodeMode
 
 
 def clickPrompt(text: str, default: Any = None, type: Optional[type] = None, **kwargs: Any) -> Any:
-    return click.prompt(click.style(f"\U00002754 {text}", fg = "blue"), default = default, type = type, **kwargs)
+    return click.prompt(click.style(f"\n\U00002754 {text}", fg = "blue"), default = default, type = type, **kwargs)
 
 
 def arrowPrompt(choices: List[Any], message: str) -> Any:
+    click.echo("\n")
     answers = inquirer.prompt([
         inquirer.List(
             "option",
@@ -43,20 +44,20 @@ def previewConfig(config: Dict[str, Any]) -> None:
 
 
 def stdEcho(text: str) -> None:
-    click.echo(click.style(text, fg = "blue"))
+    click.echo(click.style(f"\n{text}", fg = "blue"))
 
 
 def successEcho(text: str) -> None:
-    click.echo(click.style(f"\U0001F680 {text} \U0001F680", fg = "green"))
+    click.echo(click.style(f"\n\U0001F680 {text} \U0001F680", fg = "green"))
 
 
 def progressEcho(text: str) -> None:
-    click.echo(click.style(f"\U00002699 {text} \U00002699", fg = "yellow"))
+    click.echo(click.style(f"\n\U00002699 {text} \U00002699", fg = "yellow"))
 
 
 def errorEcho(text: str) -> None:
-    click.echo(click.style(f"\U0000274C {text} \U0000274C", fg = "red"))
+    click.echo(click.style(f"\n\U0000274C {text} \U0000274C", fg = "red"))
 
 
 def highlightEcho(text: str) -> None:
-    click.echo(click.style(f"\U00002755 {text} \U00002755"))
+    click.echo(click.style(f"\n\U00002755 {text} \U00002755\n"))
